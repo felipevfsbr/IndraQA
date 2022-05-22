c59b8e2689b8c02b03ce5b30a5941cc0ea518471
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-22 07:31:21 -0300

    Create Aula01-Formulario.spec.js

---
+++ Aula01-Formulario.spec.js
@@ -0,0 +1,18 @@
+///<reference types='cypress' />
+describe('teste de formulario', () => {
+    it('inputs', ()=>{
+        cy.visit('https://automacaocombatista.herokuapp.com/users/new')
+        cy.get('#user_name').type('Felipe')
+            .should('have.value', 'Felipe');
+        cy.get('#user_lastname').type('Souza')
+        cy.get('#user_email').type('felipevfsbr@gmail.com')
+        cy.get('#user_address').type('Av. Joao Pessoa')
+        cy.get('#user_university').type('Uninassau')  
+        cy.get('#user_profile').type('Estudante')
+        cy.get('#user_gender').type('Masculino')
+        cy.get('#user_age').type('29')
+        cy.get('.actions > input').click()
+        cy.get('#notice')
+            .should('have.text', 'Usuário Criado com sucesso')
+    })
+})
