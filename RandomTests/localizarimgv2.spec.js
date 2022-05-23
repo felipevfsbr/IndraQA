9c3d5bd75a54c67b1b1397cc385494fc3f05cbe8
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-23 15:28:09 -0300

    Localizando img por CSS Elements
    Localizando img por CSS Elements

--- RandomTests/localizarimgv2.spec.js
+++ RandomTests/localizarimgv2.spec.js
@@ -2,9 +2,9 @@
 describe('teste de localizacao de imagem por CSS', () => {
     it('inputs', ()=>{
         cy.visit('https://automacaocombatista.herokuapp.com/buscaelementos/links');
-        ///clicando no link e verificando texto, com CSS.
+        ///clicando no link e verificando texto, com CSS
         cy.contains('div[class="col s6 center"]', 'Ok 200 - Sucess').click()
-        ///Descobrir se o texto aparece, com CSS.
+        ///Descobrir se o texto aparece, com CSS
         cy.contains('div[class="tamanhodiv2"]', 'Success!!')
             .should('contain.text', 'Success!!')
         ///Localizar se a imagem está na pagina.
