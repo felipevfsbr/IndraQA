7f328804dac4b1b22cad47df3047fcbf5c487d64
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-23 05:02:03 -0300

    Create form01.spec.js

---
+++ RandomTests/form01.spec.js
@@ -0,0 +1,16 @@
+///<reference types='cypress' />
+describe('teste de formulario', () => {
+    it('inputs', ()=>{
+        cy.visit('https://automacaocombatista.herokuapp.com/users/new');
+        cy.get('#user_name').type('Felipe');
+        cy.get('#user_lastname').type('Souza');
+        cy.get('#user_email').type('felipevfsbr@gmail.com');
+        cy.get('#user_address').type('Av.Joao Pessoa');
+        cy.get('#user_university').type('Uninassau');
+        cy.get('#user_profile').type('Estudante');
+        cy.get('#user_gender').type('Masculino');
+        cy.get('#user_age').type('30');
+        cy.get('.actions > input').click();
+        cy.get('#notice').should('have.text', 'Usuário Criado com sucesso');
+        })
+})
